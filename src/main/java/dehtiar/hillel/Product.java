1dba385b4c725ce7c3e23bcb582313be27104f2d
Degtiar Yaroslav <merrynan2001@gmail.com>
2023-02-01 19:03:17 +0300

    created the Product

---
+++ src/main/java/dehtiar/hillel/Product.java
@@ -0,0 +1,18 @@
+package dehtiar.hillel;
+
+import lombok.Data;
+
+/**
+ * {@link Product} is a product class.
+ *
+ * @author Yaroslav Dehtiar on 01.02.2023
+ */
+@Data
+public class Product {
+
+  private String name;
+  private Integer id;
+  private double cost;
+
+
+}
